7ac83faca1415ed3aadb4af70e31494e8b70f995
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-06-14 18:33:36 -0500

    testing live reloading

--- src/recipes.ts
+++ src/recipes.ts
@@ -1,7 +1,9 @@
 export interface RecipeType {
   mead: string;
+  test: string;
 }
 
 export const recipes: RecipeType = {
   mead: "This is a mead recipe",
+  test: "this is a test of updating recipes",
 };
