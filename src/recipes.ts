5fcf25359fc48bf97f1be79b8be1fc39754520f5
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-06-15 12:23:11 -0500

    added new recipes

--- src/recipes.ts
+++ src/recipes.ts
@@ -1,9 +1,67 @@
 export interface RecipeType {
-  mead: string;
-  test: string;
+  eightminutemead: string;
+  meadmistakes: string;
+  yeasttest: string;
+  applecinnamon: string;
+  bottlecarbonation: string;
+  sweetmead: string;
+  hoppedblueberry: string;
+  carbonatedblueberry: string;
+  allstyles: string;
+  discordmead1: string;
+  discordmead2: string;
+  discordmead3: string;
+  discordmead4: string;
+  discordmead5: string;
+  discordmead6: string;
+  discordmead7: string;
+  discordmead8: string;
+  brewsysucks: string;
+  peppermintmead: string;
+  mixedberrymead: string;
+  rootbeermead: string;
+  TeaMead: string;
+  pinacolada: string;
+  redpyment: string;
+  meadmakingprocess: string;
+  whitepyment: string;
+  punishingyeast: string;
+  dndmead: string;
+  hothoney: string;
+  meadsquidgame: string;
+  cucumberlime: string;
 }
 
 export const recipes: RecipeType = {
-  mead: "This is a mead recipe",
-  test: "this is a test of updating recipes",
+  eightminutemead: "https://youtu.be/nA7tLIFcgts",
+  meadmistakes: "https://youtu.be/rCN-TSSsRmc",
+  yeasttest: "https://youtu.be/-_shVjrZIO4",
+  applecinnamon: "https://youtu.be/NF1dhaeysB0",
+  bottlecarbonation: "https://youtu.be/ipPIbB8tS0M",
+  sweetmead: "https://youtu.be/sOpBxyrANlQ",
+  hoppedblueberry: "https://youtu.be/dzUa_K-iRCY",
+  carbonatedblueberry: "https://youtu.be/yAwLpol-G6A",
+  allstyles: "https://youtu.be/08eQ-ylpvKk",
+  discordmead1: "https://youtu.be/K78YNSn5sd8",
+  discordmead2: "https://youtu.be/21OMY2OMmFg",
+  discordmead3: " https://youtu.be/v0PnWMp0COw",
+  discordmead4: "https://youtu.be/ZHfCwTogiqo",
+  discordmead5: "https://youtu.be/wrL0TTeRu0w",
+  discordmead6: "https://youtu.be/RqouA9Fl01s",
+  discordmead7: "https://youtu.be/90HIPS_dnEM",
+  discordmead8: "https://youtu.be/Todjm7wGbPU",
+  brewsysucks: "https://youtu.be/2m-EmXL8QOo",
+  peppermintmead: "https://youtu.be/6T7BdYta4YQ",
+  mixedberrymead: "https://youtu.be/0J-p54wh1tI",
+  rootbeermead: "https://youtu.be/UGuJgqhMwxU",
+  TeaMead: "https://youtu.be/3Npbhc7Jp1k",
+  pinacolada: "https://youtu.be/INJ-bxrRCBE",
+  redpyment: "https://youtu.be/jESTDN9eqhU",
+  meadmakingprocess: " https://youtu.be/NQmhIQXnnNg",
+  whitepyment: "https://youtu.be/JXit06hwAZY",
+  punishingyeast: "https://youtu.be/CsMJmDdQ-_o",
+  dndmead: "https://youtu.be/GsHlL_Suw4M",
+  hothoney: "https://youtu.be/cBZXGCQFX8I",
+  meadsquidgame: "https://youtu.be/gGD9OuZtILc",
+  cucumberlime: "https://youtu.be/hnT3RXN9v-Q",
 };
