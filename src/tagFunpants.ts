b76b25243f18daa5dc4216ebde069d3bd43113ea
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-12-16 16:16:39 -0600

    added carlos video

--- src/tagFunpants.ts
+++ src/tagFunpants.ts
@@ -4,6 +4,8 @@ const { guildId } = process.env;
 export default async function tagFunPants(client: Client) {
   const FUNPANTS = "251250155960008704";
   const STYNGER = "434942205426401322";
+  const carlosVid =
+    " https://res.cloudinary.com/devhg2clt/video/upload/v1734387048/1734386607_1734386559268_80482C2B5BB4131734386559_by9g3y.mp4";
 
   try {
     if (!guildId) throw new Error("Missing guildId in environment variables.");
@@ -30,11 +32,15 @@ export default async function tagFunPants(client: Client) {
     // Select a random channel from the filtered list
     const randomChannel = textChannels.random() as TextChannel;
 
-    // Determine whether to tag FUNPANTS or STYNGER (75% for FUNPANTS, 25% for STYNGER)
-    const tagUserId = Math.random() < 0.25 ? STYNGER : FUNPANTS;
+    const randomNum = Math.random();
 
+    // Determine whether to tag FUNPANTS or STYNGER (75% for FUNPANTS, 25% for STYNGER)
+    const tagUserId = randomNum < 0.25 ? STYNGER : FUNPANTS;
+
+    let msg = `Hi, <@${tagUserId}>`;
+    if (randomNum > 0.75) msg += carlosVid;
     // Send a message in the selected random channel
-    await randomChannel.send(`Hi, <@${tagUserId}>`);
+    await randomChannel.send(msg);
   } catch (err) {
     console.error("Error selecting or sending to a random channel", err);
     return;
