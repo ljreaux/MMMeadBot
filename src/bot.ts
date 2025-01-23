975dd1874e2478ef29004fd2368d3f7580094e2b
ljreaux <146141261+ljreaux@users.noreply.github.com>
2025-01-23 09:47:24 -0600

    edited bot chance

--- src/bot.ts
+++ src/bot.ts
@@ -54,7 +54,7 @@ cron.schedule("0 2 * * *", async () => {
 cron.schedule("*/5 * * * *", async () => await checkVideos(client));
 
 cron.schedule("0 * * * *", async () => {
-  const randomChance = Math.random() * 30;
+  const randomChance = Math.random() * 50;
   if (randomChance <= 1) {
     tagFunPants(client);
   }
