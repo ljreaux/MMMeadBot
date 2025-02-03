a7e0ecd09d801e4a601a40c0727a5b45bca8920f
ljreaux <larryreaux@gmail.com>
2025-02-03 14:26:55 +0000

    fixed funpants to be less spamy

--- src/tagFunpants.ts
+++ src/tagFunpants.ts
@@ -1,4 +1,8 @@
-import { Client, TextChannel, ChannelType, PermissionsBitField } from "discord.js";
+import {
+  Client,
+  TextChannel,
+  // ChannelType, PermissionsBitField
+} from "discord.js";
 const { guildId } = process.env;
 
 export default async function tagFunPants(client: Client) {
@@ -7,38 +11,44 @@ export default async function tagFunPants(client: Client) {
   const carlosVid =
     " https://res.cloudinary.com/devhg2clt/video/upload/v1734387048/1734386607_1734386559268_80482C2B5BB4131734386559_by9g3y.mp4";
 
+  const { botSpamChannel = "" } = process.env;
+
   try {
     if (!guildId) throw new Error("Missing guildId in environment variables.");
 
     // Fetch the guild
     const guild = await client.guilds.fetch(guildId);
 
-    // Fetch all channels in the guild
-    const channels = await guild.channels.fetch();
+    // // Fetch all channels in the guild
+    // const channels = await guild.channels.fetch();
 
-    // Filter for standard text channels where both users have access and the bot can send messages
-    const accessibleChannels = channels.filter((channel) => {
-      if (channel?.type !== ChannelType.GuildText) return false; // Ensure it's a text channel
-      const botPermissions = channel?.permissionsFor(guild.members.me!);
-      const funPantsPermissions = channel?.permissionsFor(FUNPANTS);
-      const styngerPermissions = channel?.permissionsFor(STYNGER);
+    // // Filter for standard text channels where both users have access and the bot can send messages
+    // const accessibleChannels = channels.filter((channel) => {
+    //   if (channel?.type !== ChannelType.GuildText) return false; // Ensure it's a text channel
+    //   const botPermissions = channel?.permissionsFor(guild.members.me!);
+    //   const funPantsPermissions = channel?.permissionsFor(FUNPANTS);
+    //   const styngerPermissions = channel?.permissionsFor(STYNGER);
 
-      // Check if the bot and both users have permission to view and send messages in the channel
-      return (
-        botPermissions?.has(PermissionsBitField.Flags.SendMessages) &&
-        funPantsPermissions?.has(PermissionsBitField.Flags.ViewChannel) &&
-        styngerPermissions?.has(PermissionsBitField.Flags.ViewChannel)
-      );
-    });
+    //   // Check if the bot and both users have permission to view and send messages in the channel
+    //   return (
+    //     botPermissions?.has(PermissionsBitField.Flags.SendMessages) &&
+    //     funPantsPermissions?.has(PermissionsBitField.Flags.ViewChannel) &&
+    //     styngerPermissions?.has(PermissionsBitField.Flags.ViewChannel)
+    //   );
+    // });
 
-    console.log(accessibleChannels.map((channel) => channel?.name));
+    // console.log(accessibleChannels.map((channel) => channel?.name));
 
-    if (!accessibleChannels.size) {
-      throw new Error("No suitable text channels available.");
-    }
+    // if (!accessibleChannels.size) {
+    //   throw new Error("No suitable text channels available.");
+    // }
 
     // Select a random channel from the filtered list
-    const randomChannel = accessibleChannels.random() as TextChannel;
+    const randomChannel = client.channels.cache.get(
+      botSpamChannel
+    ) as TextChannel;
+
+    // accessibleChannels.random() as TextChannel;
 
     const randomNum = Math.random();
 
