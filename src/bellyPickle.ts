d13f757afd956daf644e2090caed309dd6acfa6f
ljreaux <146141261+ljreaux@users.noreply.github.com>
2025-04-07 22:15:15 -0500

    update regex

--- src/bellyPickle.ts
+++ src/bellyPickle.ts
@@ -1,7 +1,7 @@
 import { Message, PartialMessage, TextChannel } from "discord.js";
 
 const regex =
-  /belly.*pickle|pickle.*belly|pelly|bickle|sbp|s.b.p|belly.*cucumber|cucumber.*belly|tummy.*pickle|pickle.*tummy|pouthern|bouthern/i;
+  /belly.*pickle|pickle.*belly|pelly|bickle|sbp|s\.b\.p|belly.*cucumber|cucumber.*belly|tummy.*pickle|pickle.*tummy|pouthern|bouthern/i;
 
 export const handleHooligans = async (
   message: Message | Message<boolean> | PartialMessage
